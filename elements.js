9ca12ceea94ffbc339588f2a888e6fe72ed48eec
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-20 14:47:31 +0200

    commented out elements.js file

--- elements.js
+++ elements.js
@@ -36,6 +36,7 @@
  */
 export const htmlElements = {
     list: {
+        // Selecting list-related HTML elements
         dataListItem: document.querySelector('[data-list-items]'),
         dataListButton: document.querySelector('[data-list-button]'),
         dataListClose: document.querySelector('[data-list-close]'),
@@ -48,10 +49,12 @@ export const htmlElements = {
         dataListDescription: document.querySelector('[data-list-description]')
     },
     header: {
+        // Selecting header-related HTML elements
         dataHeaderSearch: document.querySelector('[data-header-search]'),
         dataHeaderSetting: document.querySelector('[data-header-settings]')
     },
     search: {
+        // Selecting search-related HTML elements
         dataSearchAuthor: document.querySelector('[data-search-authors]'),
         dataSearchCancel: document.querySelector('[data-search-cancel]'),
         dataSearchGenre: document.querySelector('[data-search-genres]'),
@@ -60,6 +63,7 @@ export const htmlElements = {
         dataSearchForm: document.querySelector('[data-search-form]')
     },
     setting: {
+        // Selecting setting-related HTML elements
         dataSettingTheme: document.querySelector('[data-settings-theme]'),
         dataSettingCancel: document.querySelector('[data-settings-cancel]'),
         dataSettingOverlay: document.querySelector('[data-settings-overlay]'),
