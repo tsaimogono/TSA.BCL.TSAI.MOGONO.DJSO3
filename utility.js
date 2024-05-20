0583436c2080f90eb415710dea88c2ca798620e2
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-20 13:38:47 +0200

    exports a function createBookElement that creates and returns an HTML button element representing a book

--- utility.js
+++ utility.js
@@ -10,3 +10,21 @@ import { htmlElements } from './elements.js';
  * @param {string} book.title - The title of the book.
  * @returns {HTMLElement} The book element.
  */
+export const createBookElement = (book) => {
+    const { author, id, image, title } = book;
+    const element = document.createElement('button');
+    element.classList = 'preview';
+    element.setAttribute('data-preview', id);
+    element.innerHTML = `
+        <img
+            class="preview__image"
+            src="${image}"
+        />
+            
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${authors[author]}</div>
+        </div>
+    `;
+    return element;
+}
